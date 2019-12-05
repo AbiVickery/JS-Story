67cfb18355b23b062deacbaa488f097db5664944
Abi Vickery <vickeryabi@gmail.com>
2019-12-05 14:56:11 -0600

    2 functions

--- js/app.js
+++ js/app.js
@@ -6,5 +6,26 @@
 // var dalek = 'Dalek';
 
 function choice1(data) {
-    
+    var c1 = 'Choice1';
+    choice1 = data;
+    var c2 = 'Choice2';
+    choice2 = data;
+    if (c1 > c2) {
+        console.log("Choice 1");
+    } else {
+        console.log("Choice 2");
+    }
+    return choice2;
 }
+
+function choice2(data) {
+    if (c1 < c2) {
+        console.log("Choice 2");
+    } else {
+        console.log("Choice 1");
+    }
+    return choice1;
+}
+
+choice1();
+choice2();
