ade77411264adf06e77e78afc5b38afd4fdb673a
Abi Vickery <vickeryabi@gmail.com>
2019-12-06 13:59:03 -0600

    a story from the pov of a rock

--- js/app.js
+++ js/app.js
@@ -94,3 +94,48 @@
 
 // Starting fresh!
 
+// The adventures of a rock
+// different rocks to choose from: {average pebble, smooth small rock (like in nice flower beds), gemstone[choose from Ruby, diamond or topaz], a rock that looks like a rock but is actually really old petrified chewed gum.}
+
+// things the game will have:
+// {rock type choice}
+// {at least 4 choices moving forward}
+// {3 different scenarioes}
+
+var character = {
+    // health: 'NA',
+    name: 'rock',
+    type: ['Pebble', 'rock', 'Ruby', 'Diamond', 'topaz', 'Petrified gum']
+}
+var monster = {
+    mon1 = 'A human',
+    mon2 = 'A boot',
+    mon3 = 'A bird',
+    mon4 = 'A river'
+}
+
+function lilrock(payload) {
+    let avatar = alert("Choose who you are! (your type of rock)");
+    console.log('Choosing avatar');
+    ans = prompt('A for Pebble ,' + payload.rock1 + 'B for Rock ,' + payload.rock2 + 'C for Ruby ,' + payload.rock3 + 'D for Diamond ,' + payload.rock4 + 'E for Topaz ' + payload.rock5 + 'And F for Petrified Gum.');
+
+    if (ans == 'A') {
+        console.log(character.type[0]);
+        document.write('You are a Pebble. You are sweet and tiny. So cute!');
+    } else if (ans == 'B') {
+        console.log(character.type[1]);
+        document.write('You are a rock, just a plain \'ol rock, not that special...');
+    } else if (ans == 'C') {
+        console.log(character.type[2]);
+        document.write('You are a Ruby. A beautiful red color, hard, and tough.');
+    } else if (ans == 'D') {
+        console.log(character.type[3]);
+        document.write('You are a Diamond. You shine brightly and colorfully! You are hard under pressure! But can still shatter so be carefull.');
+    } else if (ans == 'E') {
+        console.log(character.type[4]);
+        document.write('You are a Topaz. You have a nice shine, you\'re tough and do what you want!');
+    } else if (ans == 'F') {
+        console.log(character.type[5]);
+        document.write('You are Petrified Gum... You used to be a stick of sugar but were than chewed up, spat out and sat on a street for so long, you ended up just like those historical Petrified logs of wood you would find in a museum...');
+    }
+}
