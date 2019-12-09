d8f59f105fff91be6353b8073a06b06572fb4063
Abi Vickery <vickeryabi@gmail.com>
2019-12-09 15:27:48 -0600

    README.md
    some of the old code that was commented out was creating errors for some strange reason so I moved it over to the readme file, so I will still have it around and easy to access

--- js/app.js
+++ js/app.js
@@ -1,96 +1,3 @@
-// // build your own adventure
-// // at least four stages
-
-// /*  var dr = 'The Doctor';
-//     var spk = 'Sparkly Keys';
-//     var dalek = 'Dalek';     */
-
-// function choice1(data) {
-//     var c1 = 'Choice1';
-//     choice1 = data;
-//     var c2 = 'Choice2';
-//     choice2 = data;
-//     if (c1 > c2) {
-//         console.log("Choice 1");
-//     } else {
-//         console.log("Choice 2");
-//     }
-//     return choice2;
-// }
-
-// function choice2(data) {
-//     if (c1 < c2) {
-//         console.log("Choice 2");
-//     } else {
-//         console.log("Choice 1");
-//     }
-//     return choice1;
-// }
-
-// choice1();
-// choice2();
-
-// class Plantly {
-//     constructor(name) {
-//         this.name = name;
-//     }
-//     getName() {
-//         console.log(this.name);
-//     }
-// }
-
-// var rose = new Plantly('Rose');
-// var tulip =  new Plantly('Tulip');
-// var mum = new Plantly('Chrysanthemum');
-// var daff = new Plantly('Daffodil');
-// var snap = new Plantly('Snap Dragon');
-// var alg = new Plantly('Algae');
-// var liv = new Plantly('Liverworts');
-// var moss = new Plantly('Moss');
-// var fern = new Plantly('Fern');
-// var hort = new Plantly('Horsetail');
-
-// rose.getName();
-// tulip.getName();
-// mum.getName();
-// daff.getName();
-// snap.getName();
-// alg.getName();
-// liv.getName();
-// moss.getName();
-// fern.getName();
-// hort.getName();
-
-// class Rockly {
-//     constructor(name) {
-//         this.name = name;
-//     }
-//     getName() {
-//         console.log(this.name);
-//     }
-// }
-
-// var quart = new Rockly('Quartz');
-// var coal = new Rockly('Coal');
-// var jas = new Rockly('Jasper');
-// var emer = new Rockly('Emerald');
-// var dia = new Rockly('Diamond');
-// var rutile = new Rockly('Rutile');
-// var phos = new Rockly('Phosphopylite');
-// var cinn = new Rockly('Cinnabar');
-// var bis = new Rockly('Bismuth');
-
-// quart.getName();
-// coal.getName();
-// jas.getName();
-// emer.getName();
-// dia.getName();
-// rutile.getName();
-// phos.getName();
-// cinn.getName();
-// bis.getName();
-
-
 
 // Starting fresh!
 
@@ -160,6 +67,8 @@ function lilrock(payload) {
     }
 
  */
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* Scenario 2:
 
